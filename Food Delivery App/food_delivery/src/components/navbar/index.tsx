bbdc590218d0c1f088f7b03fb50d53d66c6e3690
Kasheesh PrabhuDesai <kasheesh171995@gmail.com>
2022-02-22 06:51:38 +0100

    ADDED global styles to improve the font family and font size of the navbar text

--- Food Delivery App/food_delivery/src/components/navbar/index.tsx
+++ Food Delivery App/food_delivery/src/components/navbar/index.tsx
@@ -1,17 +1,43 @@
-import { AppBar, Box, IconButton, Toolbar, Typography, Button } from "@material-ui/core";
+import { AppBar, Box, IconButton, Toolbar, Typography, Button, makeStyles, createStyles } from "@material-ui/core";
+import { mergeClasses } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme =>
+    createStyles({
+        navBarCompanyText: {
+            fontFamily: "verdana",
+            fontSize: 24,
+            fontWeight: 500
+        },
+        navBarLinkText: {
+            fontFamily: "verdana",
+            fontSize: 16,
+            fontWeight: 500
+        }
+    })
+);
 
 export default function NavBar() {
+    const classes = useStyles();
+
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" color={"primary"}>
+            <AppBar position="fixed" color={"primary"}>
                 <Toolbar>
-                    <Typography variant="h4" component="div" style={{ flexGrow: 1 }}>
+                    <Typography style={{ flexGrow: 1 }} className={classes.navBarCompanyText}>
                         Wheels on Meals
                     </Typography>
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">About Us</Button>
-                    <Button color="inherit">Menu</Button>
-                    <Button color="inherit">Contact Us</Button>
+                    <Button color="inherit">
+                        <Typography className={classes.navBarLinkText}>Home</Typography>
+                    </Button>
+                    <Button color="inherit">
+                        <Typography className={classes.navBarLinkText}>About Us</Typography>
+                    </Button>
+                    <Button color="inherit">
+                        <Typography className={classes.navBarLinkText}>Menu</Typography>
+                    </Button>
+                    <Button color="inherit">
+                        <Typography className={classes.navBarLinkText}>Contact Us</Typography>
+                    </Button>
                 </Toolbar>
             </AppBar>
         </Box>
