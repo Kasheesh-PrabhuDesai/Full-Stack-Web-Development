69c17275328cce3d1899cf6377c37b0cb505f4d4
Kasheesh PrabhuDesai <kasheesh171995@gmail.com>
2022-04-23 09:42:48 +0200

    REFACTORED _app.tsx and _document.tsx to prevent loss of material ui styles on referesh

--- Amazon Clone/amazon-clone/pages/_app.tsx
+++ Amazon Clone/amazon-clone/pages/_app.tsx
@@ -1,24 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useEffect } from 'react';
+import "../styles/globals.css";
+import { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from "../src/theme";
 
-
-function MyApp({ Component, pageProps }: AppProps) {
-  
+function MyApp({ Component, pageProps }: any) {
   useEffect(() => {
-      // Remove the server-side injected CSS.
-      const jssStyles = document.querySelector("#jss-server-side");
-      if (jssStyles && jssStyles.parentElement) jssStyles.parentElement.removeChild(jssStyles);
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) jssStyles?.parentElement?.removeChild(jssStyles);
   }, []);
-  
-  return <Component {...pageProps} />
+
+  return (
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   emotionCache: PropTypes.object,
-  pageProps: PropTypes.object.isRequired
+  pageProps: PropTypes.object.isRequired,
 };
--- Amazon Clone/amazon-clone/pages/_document.tsx
+++ Amazon Clone/amazon-clone/pages/_document.tsx
@@ -1,40 +1,38 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-// import theme from "../src/theme";
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    {/* PWA primary color */}
-                    {/*<meta name="theme-color" content={theme.palette.primary.main} />*/}
-                    <link rel="icon" type="image/png" href="/favicon.png" />
-                    {/*<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />*/}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 MyDocument.getInitialProps = async ctx => {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () =>
-        originalRenderPage({
-            enhanceApp: App => props => sheets.collect(<App {...props} />)
-        });
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />),
+    });
+  };
 
-    const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx);
 
-    return {
-        ...initialProps,
-        styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()]
-    };
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
 };
