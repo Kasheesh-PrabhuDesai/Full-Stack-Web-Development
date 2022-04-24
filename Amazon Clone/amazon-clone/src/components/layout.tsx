4a276aa66a1dc54200f73af6b6dab51bc9e6c3e8
Kasheesh PrabhuDesai <kasheesh171995@gmail.com>
2022-04-24 13:59:01 +0200

    ADDED two list items to the app navbar and created a custom theme to support light mode and dark mode change depending on a switch

--- Amazon Clone/amazon-clone/src/components/layout.tsx
+++ Amazon Clone/amazon-clone/src/components/layout.tsx
@@ -1,27 +1,99 @@
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Box,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+} from "@material-ui/core";
 import Head from "next/head";
-import React from "react";
+import React, { useContext, useState } from "react";
 import useStyles from "../utils/style";
+import NextLink from "next/link";
+import { Store } from "../store";
+import Cookies from "js-cookie";
 
-export default function Layout({ children }) {
+export default function Layout({ children, description, title }: any) {
   const classes = useStyles();
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+
+  console.log(darkMode);
+
+  const [pageMode, setPageMode] = useState(false);
+
+  const handleChangeMode = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      body1: {
+        fontWeight: "normal",
+      },
+    },
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
 
   return (
-    <div>
+    <Box>
       <Head>
-        <title>Lets Shop</title>
+        <title>{title ? `${title}-Lets Shop` : "Lets Shop"}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navBar}>
-        <Toolbar>
-          <Typography className={classes.navBarText}>Lets Shop</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography variant="h4">
-          All rights reserved. Lets Shop ©2022-2023
-        </Typography>
-      </footer>
-    </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link className={classes.grow}>
+                <Typography className={classes.brand}>Lets Shop</Typography>
+              </Link>
+            </NextLink>
+            <Switch checked={darkMode} onClick={handleChangeMode}></Switch>
+            <NextLink href="/cart" passHref>
+              <Link>
+                <Typography>Cart</Typography>
+              </Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>
+                <Typography>Login</Typography>
+              </Link>
+            </NextLink>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography variant="h4">
+            All rights reserved. Lets Shop ©2022-2023
+          </Typography>
+        </footer>
+      </ThemeProvider>
+    </Box>
   );
 }
