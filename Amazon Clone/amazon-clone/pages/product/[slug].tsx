2738c20de971adff8ff4e9bfdbca0ab90940ccbf
Kasheesh PrabhuDesai <kasheesh171995@gmail.com>
2022-04-30 12:02:00 +0530

    ADDED server side props to product slug to get each individual product from server

--- Amazon Clone/amazon-clone/pages/product/[slug].tsx
+++ Amazon Clone/amazon-clone/pages/product/[slug].tsx
@@ -7,23 +7,28 @@ import {
   Card,
   Button,
 } from "@material-ui/core";
-import { useRouter } from "next/router";
 import Layout from "../../src/components/layout";
-import data from "../../src/utils/data";
 import NextLink from "next/link";
 import useStyles from "../../src/utils/style";
 import Image from "next/image";
+import db from "../../src/utils/connectDb";
+import Product from "../../src/models/Product";
+import { useContext } from "react";
+import { Store } from "../../src/store";
 
-export default function ProductPage() {
-  const router = useRouter();
-
+export default function ProductPage(props: any) {
+  const { product } = props;
   const classes = useStyles();
 
-  const { slug } = router.query;
-  const product = data.products.find(a => a.slug === slug);
+  const { state, dispatch } = useContext(Store);
+
   if (!product) {
     return <div>Product Not Found</div>;
   }
+
+  const handleAddToCart = async () => {
+    dispatch({ type: "ADD_TO_CART", payload: { ...product, quantity: 1 } });
+  };
 
   return (
     <Layout title={product.name} description={product.description}>
@@ -63,7 +68,7 @@ export default function ProductPage() {
               </Typography>
             </ListItem>
             <ListItem>
-              <Typography>Description: {product.category}</Typography>
+              <Typography>Description: {product.description}</Typography>
             </ListItem>
           </List>
         </Grid>
@@ -93,7 +98,12 @@ export default function ProductPage() {
                 </Grid>
               </ListItem>
               <ListItem>
-                <Button fullWidth variant="contained" color="primary">
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={handleAddToCart}
+                >
                   Add to cart
                 </Button>
               </ListItem>
@@ -103,4 +113,17 @@ export default function ProductPage() {
       </Grid>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const { params } = context;
+  const { slug } = params;
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
 }
