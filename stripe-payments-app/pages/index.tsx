2ae7bd618294eeefaeb019f550cc63fec5f2ebb8
Kasheesh <kasheesh171995@gmail.com>
2022-06-05 10:30:16 +0200

    MOVED checkout file as it is not react component to outside pages

--- stripe-payments-app/pages/index.tsx
+++ stripe-payments-app/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { checkout } from "./checkout";
+import { checkout } from "../checkout";
 
 const Home: NextPage = () => {
   const handleCheckout = () => {};
